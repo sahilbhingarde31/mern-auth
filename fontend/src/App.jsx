75255026376a7f812027a821c21a2ca046c43839
Sahil Bhingarde <sahilbhingarde.sb31@gmail.com>
2025-05-17 22:07:32 +0530

    frontend setup and added signup, login routes

---
+++ fontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import FloatingShape from "./components/floatingShape"
+import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0}/> {/*with Floating shape we passing props(properties) */}
+      <FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5}/>{/*with Floating shape we passing props(properties) */}
+      <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2}/>{/*with Floating shape we passing props(properties) */}
+
+      <Routes>
+        <Route path="/" element={"Home"}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
