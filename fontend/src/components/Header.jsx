6f9d0540a25e0dbfab44a7547700504618787d7c
Sahil Bhingarde <sahilbhingarde.sb31@gmail.com>
2025-06-09 22:31:22 +0530

    add company logo

--- fontend/src/components/Header.jsx
+++ fontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div className='bg-gradient-to-r from-green-600 to-emerald-700 sticky top-0 backdrop-blur-md shadow-md z-50'>
       <div className="flex items-center justify-between max-w-6xl mx-auto p-3">
         <Link to='/'>
-          <img src="./assest/shield.png" alt="companylogo" className="w-20 h-15" />
+          <img src="shield.png" alt="" className="size-10 rounded-lg" />
         </Link>
         <h1 className="text-2xl font-bold bg-gradient-to-r from-green-300 to-emerald-400 text-transparent bg-clip-text">Auth Company</h1>
 
